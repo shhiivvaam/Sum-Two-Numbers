7416d3afe8c99a45f86f1bd8892aae12987047ca
Shivam Kumar <life.shivam2394@gmail.com>
2023-12-13 10:35:15 +0530

    Application Build Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [sum, setSum] = useState(null);
+
+  const calculateSum = () => {
+    const result = parseFloat(num1) + parseFloat(num2);
+    setSum(result);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sum Calculator</h1>
+      <div className="input-container">
+        <input
+          type="number"
+          placeholder="Enter number 1"
+          value={num1}
+          onChange={(e) => setNum1(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Enter number 2"
+          value={num2}
+          onChange={(e) => setNum2(e.target.value)}
+        />
+      </div>
+      <button onClick={calculateSum}>Calculate Sum</button>
+      {sum !== null && <p className="result">Sum: {sum}</p>}
     </div>
   );
 }
